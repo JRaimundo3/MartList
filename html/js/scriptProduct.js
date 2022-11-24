f15a2397ef83045e9300431bbb2da14f7b16be74
aposilva <apo.silva@campus.fct.unl.pt>
2022-11-24 05:12:03 +0000

    variables from fetch shown

--- html/js/scriptProduct.js
+++ html/js/scriptProduct.js
@@ -8,34 +8,34 @@ fetch('https://fakestoreapi.com/products?limit=0') // Para a pag principal escol
   .then(res => res.json())
   .then(data => {
     data.forEach(product => {
-      const card = productCardTemplate.content.cloneNode(true).children[0]
-      const header = card.querySelector("[data-header]")
-      const body = card.querySelector("[data-body]")
-      header.textContent = product.title
-      body.textContent = product.price
-      productCardContainer.append(card)
+      const card = productCardTemplate.content.cloneNode(true).children[0] 
+      const header = card.querySelector("[data-header]") 
+      const body = card.querySelector("[data-body]") 
+      const img = card.querySelector("[data-img]") 
+      const id = card.querySelector("[data-name]")
+      const description = card.querySelector("[data-description]")
+      header.textContent = product.title 
+      body.textContent = product.price + "€"
+      img.textContent = product.image
+      //id.textContent = product.id
+      //description.textContent = product.description   //dao erro a ir buscar 
+      productCardContainer.append(card) 
     });
 
   })
 
 
-
-
-
-
-
-/*
 let preveiwContainer = document.querySelector('.products-preview');
 let previewBox = preveiwContainer.querySelectorAll('.preview');
 
 document.querySelectorAll('.products-container .product').forEach(product =>{
   product.onclick = () =>{
-    preveiwContainer.style.display = 'flex';
+    preveiwContainer.style.display = 'flex'; //para aparecer o preview
     let name = product.getAttribute('data-name');
     previewBox.forEach(preview =>{
       let target = preview.getAttribute('data-target');
       if(name == target){
-        preview.classList.add('active');
+        preview.classList.add('active'); 
       }
     });
   };
@@ -43,7 +43,7 @@ document.querySelectorAll('.products-container .product').forEach(product =>{
 
 previewBox.forEach(close =>{
   close.querySelector('.fa-times').onclick = () =>{
-    close.classList.remove('active');
+    close.classList.remove('active');  //para fechar o preview
     preveiwContainer.style.display = 'none';
   };
-});*/
+});
