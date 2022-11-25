ae2240da762a1a452640d4481c176c83b7b79562
aposilva <apo.silva@campus.fct.unl.pt>
2022-11-25 17:41:17 +0000

    images now show

--- html/js/scriptProduct.js
+++ html/js/scriptProduct.js
@@ -4,27 +4,28 @@ const productCardContainer = document.querySelector("[data-product-cards-contain
 
 
 
+
 fetch('https://fakestoreapi.com/products?limit=0') // Para a pag principal escolher um limite, neste momento está a 0 porque não sei porque quando retornar produtos lixa o menu 
   .then(res => res.json())
   .then(data => {
     data.forEach(product => {
       const card = productCardTemplate.content.cloneNode(true).children[0] 
-      const header = card.querySelector("[data-header]") 
+      const header = card.querySelector("[data-header]") //vai buscar ao html o elemento com o data-header
       const body = card.querySelector("[data-body]") 
-      const img = card.querySelector("[data-img]") 
+      const img = card.querySelector("[data-img]")  //vai buscar ao html o elemento com o data-img
       const id = card.querySelector("[data-name]")
       const description = card.querySelector("[data-description]")
       header.textContent = product.title 
       body.textContent = product.price + "€"
-      img.textContent = product.image
-      //id.textContent = product.id
+      img.src = product.image;
+      id.textContent = product.id
       //description.textContent = product.description   //dao erro a ir buscar 
-      productCardContainer.append(card) 
+      productCardContainer.append(card)   
     });
 
   })
 
-
+/*
 let preveiwContainer = document.querySelector('.products-preview');
 let previewBox = preveiwContainer.querySelectorAll('.preview');
 
@@ -47,3 +48,4 @@ previewBox.forEach(close =>{
     preveiwContainer.style.display = 'none';
   };
 });
+*/
