8b20fd50ebd1ddb2bdfec2469bdcd8e6de892809
aposilva <apo.silva@campus.fct.unl.pt>
2022-11-28 02:44:57 +0000

    single product page done

--- html/js/scriptProduct.js
+++ html/js/scriptProduct.js
@@ -2,6 +2,7 @@
 const productCardTemplate = document.querySelector("[data-product-template]") 
 const productCardContainer = document.querySelector("[data-product-cards-container]") 
 const searchInput = document.querySelector("[data-search]") 
+let xmlhttp = new XMLHttpRequest();
 
 
 
@@ -15,19 +16,42 @@ fetch('https://fakestoreapi.com/products?limit=0') // Para a pag principal escol
   .then(res => res.json())
   .then(data => {
     data.forEach(product => {
+
       const card = productCardTemplate.content.cloneNode(true).children[0] 
       const header = card.querySelector("[data-header]") //vai buscar ao html o elemento com o data-header
       const body = card.querySelector("[data-body]") 
       const img = card.querySelector("[data-img]")  //vai buscar ao html o elemento com o data-img
-      const description = card.querySelector("[data-description]")
+      const id = card.querySelector("[data-id]")  //vai buscar ao html o elemento com o data-img
       header.textContent = product.title 
       body.textContent = product.price + "€"
       img.src = product.image;
+      id.href = "product.html?"+product.id;
       //description.textContent = product.description   //dao erro a ir buscar 
       productCardContainer.append(card)   
     });
 
   })
+
+  function processRegister() {
+
+    let objJSON = JSON.stringify(obj);
+    let xmlhttp = new XMLHttpRequest();
+
+    if (xmlhttp) {
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState === 4) {
+                if (xmlhttp.status === 200) {
+                    //window.location.replace(".html");
+                } else {
+                    console.log(xmlhttp.status);
+                }
+            }
+        }
+        xmlhttp.open("POST", url, true);
+        xmlhttp.setRequestHeader("Content-Type", "application/json");
+        xmlhttp.send(objJSON);
+    }
+}
 
 /*
 let preveiwContainer = document.querySelector('.products-preview');
